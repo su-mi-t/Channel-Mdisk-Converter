02be6d2bceb440ec0b94b739dd03a3773d5ac62f
su-mi-t <81008992+su-mi-t@users.noreply.github.com>
2023-01-12 16:24:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,20 +10,20 @@ def is_enabled(value, default):
 
 # Mandatory variables for the bot to start
 API_ID = int(os.environ.get("API_ID", "23429472"))
-API_HASH = os.environ.get("API_HASH", "e19f8efefae22654d25460990814aeec)
+API_HASH = os.environ.get("API_HASH", "e19f8efefae22654d25460990814aeec")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "5886140573:AAG57hjHVx_z9AJ2o-hKcASrvzjZsvCq_l4")
 DROPLINK_API = os.environ.get("DROPLINK_API")
-MDISK_API = os.environ.get("MDISK_API", "vWQzPKz0noK2p4x89ewR)
+MDISK_API = os.environ.get("MDISK_API", "vWQzPKz0noK2p4x89ewR")
 ADMINS = list(int(i.strip()) for i in os.environ.get("ADMINS").split(",")) if os.environ.get("ADMINS") else []
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "MdiskConvertorBotpro")
-DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Movies:<password>@cluster0.ttvkebv.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Movies:movies@cluster0.ttvkebv.mongodb.net/?retryWrites=true&w=majority")
 WEBSITE = os.environ.get('WEBSITE')
 
 #  Optionnal variables
 INCLUDE_DOMAIN = list(i.strip() for i in os.environ.get("INCLUDE_DOMAIN").split(",")) if os.environ.get("INCLUDE_DOMAIN") else []
 EXCLUDE_DOMAIN = list(i.strip() for i in os.environ.get("EXCLUDE_DOMAIN").split(",")) if os.environ.get("EXCLUDE_DOMAIN") else []
 CHANNELS = is_enabled((os.environ.get('CHANNELS', "True")), True)
-CHANNEL_ID = list(int(i.strip()) for i in os.environ.get("CHANNEL_ID").split("-1001806065563")) if os.environ.get("CHANNEL_ID") else []
+CHANNEL_ID = list(int(i.strip()) for i in os.environ.get("CHANNEL_ID").split("-1001732061586")) if os.environ.get("CHANNEL_ID") else []
 FORWARD_MESSAGE = is_enabled((os.environ.get('FORWARD_MESSAGE', "True")), True)
 SOURCE_CODE = os.environ.get("SOURCE_CODE", "https://github.com/CyniteOfficial/Channel-Mdisk-Converter")
 USERNAME = os.environ.get("USERNAME", "CyniteBackup")
